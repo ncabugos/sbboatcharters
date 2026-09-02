452be861e6527f3dda604bfd1adb6f5b5a815068
Nick Cabugos <cabugosb3@gmail.com>
2026-09-02 06:25:34 -0700

    Revert "Show bank payment first at booking checkout"
    This reverts commit e29f59bf6495e47d46b53c3eaa7ef7758a7f3750.

--- app/book/[tour]/BookingFlow.jsx
+++ app/book/[tour]/BookingFlow.jsx
@@ -520,9 +520,7 @@ function PaymentStep({ checkout, onError, gaItem, party }) {
   return (
     <div className={styles.payBox}>
       <ExpressCheckoutElement onConfirm={confirm} options={{ buttonHeight: 48 }} />
-      {/* Bank (ACH) first: lowest fees for us and Stripe shows a cash-back
-          offer on it, so it is the best-value option for the guest too. */}
-      <PaymentElement options={{ layout: 'tabs', paymentMethodOrder: ['us_bank_account', 'card'] }} />
+      <PaymentElement options={{ layout: 'tabs' }} />
       <button
         type="button"
         className="btn btn--primary btn--large"
